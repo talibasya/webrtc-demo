330faedbde8aec758834236f357d3834fa2dbc7e
Michał Łaszczewski <michal@emikse.com>
2018-01-22 10:51:34 +0100

    ecosystem

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+npm install
+pushd client
+  webpack
+popd
---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+const db_name = "semgregate"
+
+const env =  {
+  BASE_HREF: 'http://localhost:8701',
+  HTTP_PORT: 8181
+}
+
+const env_production = {
+  ...env,
+  BASE_HREF: 'http://comm.xaos.ninja',
+  HTTP_PORT: 8004
+}
+
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    {
+      name      : 'server',
+      script    : 'index.js',
+      cwd       : 'server',
+      env,
+      env_production
+    }
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : 'comm-xaos.default.svc.cluster.local',
+      ref  : 'origin/master',
+      repo : 'https://bitbucket.org/MLaszczewski/webrtc-demo',
+      path : '/root',
+      'post-deploy' : './compile.sh && pm2 reload ecosystem.config.js --env production',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
