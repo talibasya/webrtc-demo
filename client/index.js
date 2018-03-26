97b9f55e64cdd37ecf679f854a30d26c9c61a129
Michał Łaszczewski <michal@emikse.com>
2018-03-26 11:12:52 +0200

    fix

--- client/index.js
+++ client/index.js
@@ -199,10 +199,10 @@ view.showLoading("Waiting for video input.", "Please connect camera.")
 let userMediaSettings = { audio: true, video: true }
 
 let userMediaPromise
-if(navigator.mediaDevices.getUserMedia) userMediaPromise = navigator.mediaDevices.getUserMedia(userMediaSettings)
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) userMediaPromise = navigator.mediaDevices.getUserMedia(userMediaSettings)
 else if(navigator.getUserMedia) userMediaPromise = new Promise(function(resolve, reject) {
   navigator.getUserMedia(userMediaSettings, resolve, reject)
-})
+}) else throw new Error("getUserMedia not available")
 userMediaPromise
   .then(function(stream) {
     localStream = stream
